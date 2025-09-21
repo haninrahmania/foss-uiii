0b6d278d2440b4aaaea14595c3dc1a6e69eb5db3
haninrahmania <hanin.atina@ui.ac.id>
2025-09-21 12:56:45 +0700

    added s3 bucket for media

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,4 +1,7 @@
 import sharp from "sharp";
+import { s3Adapter } from '@payloadcms/plugin-cloud-storage/s3';
+import { s3Storage } from '@payloadcms/storage-s3'
+
 import { lexicalEditor } from "@payloadcms/richtext-lexical";
 import { postgresAdapter } from "@payloadcms/db-postgres";
 import { buildConfig } from "payload";
@@ -150,4 +153,21 @@ export default buildConfig({
   typescript: {
     outputFile: "./payload-types.ts",
   },
+
+  plugins: [
+    s3Storage({
+      collections: {
+        media: true,    // or your slug for media collection
+      },
+      bucket: process.env.R2_BUCKET_NAME!,
+      config: {
+        credentials: {
+          accessKeyId: process.env.R2_ACCESS_KEY_ID!,
+          secretAccessKey: process.env.R2_SECRET_ACCESS_KEY!,
+        },
+        endpoint: process.env.R2_ENDPOINT!,   // configurate for R2
+        region: 'auto',                        // as needed
+      },
+    }),
+  ],
 });
